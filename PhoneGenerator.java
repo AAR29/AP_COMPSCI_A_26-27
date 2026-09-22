9b909c82b3809fbc032d7a2a5d1aceb84fcbef15
AAR29 <aramos2027@cchsdons.com>
2026-09-22 12:15:41 -0700

    Add files via upload

--- PhoneGenerator.java
+++ PhoneGenerator.java
@@ -7,38 +7,9 @@ package phonegenerator;
 import java.util.Random;
 import java.util.Scanner;
 import java.text.DecimalFormat;
-/**
- *
- * Part 1 - Phone Number Generator
-
-Plan and write a program that generates random phone numbers in the pattern 
-* XXX-XXX-XXXX. Include the dashes in the output.
-
-Do not let the first 3 digits contain the numbers 8 or 9 
-* (but don't be any more restrictive than that).
-
-Make sure the second set of 3 digits does not go higher than 655 
-(so 000 to 655 OK, 656 not OK).
-
-Recall that all 3 sets can have the digit 0 anywhere in the set 
-(so 071-010-0210 is fine).
-
-Think through the easiest way to construct the phone number. 
-* Each of the 10 digits does not have to be determined individually.
-
-For a bonus point, place it inside a loop, 
-* and when the user presses the letter "p", it will generate a new one.
-
-Part 2 - 6 sided Die generator
-
-Plan and write a program that randomly generates an integer from 1 to 6.
-
-for a bonus point, put it inside a loop, 
-* and each time the user types the letter "r", it "rolls the dice" and 
-* generates another random number, from 1 to 6.
-
-Keep the die generator in mind, as it will come into use in the next unit.
- */
+/*
+ Part 1 - Phone Number Generator
+*/
 public class PhoneGenerator {
 
     public static void main(String[] args) {
@@ -61,12 +32,16 @@ public class PhoneGenerator {
             
             //next 3
             int secondSample = rand.nextInt(656);
-            DecimalFormat df = new DecimalFormat("000");
+            DecimalFormat df = new DecimalFormat("000"); //making a decimal format variable 
             String s3 = df.format(secondSample);
+            /*
+            Decimal format is a package where we can reformat our numbers
+            000 ensures there is always a 0 at the beginning in absence of a hundreds/tens place
+            */
             
                     
             //final 3
-            int finalSample = rand.nextInt(10000);
+            int finalSample = rand.nextInt(10000); //final number 9999
             DecimalFormat dg = new DecimalFormat("0000");
             String f4 = dg.format(finalSample);
 
